f793cc53ec9b563820cff193a00aa535785da2c2
ekondrashov <Egorkondr4@gmail.com>
2021-11-02 01:03:34 +0300

    added information about dynamic arrays

---
+++ dynamic memory/arrays/array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{   
+    int size;
+    std::cin >> size;
+    int *arr = new int[size];
+
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10;
+    }
+    
+
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << "\t";
+        std::cout << arr + i << "\n";
+    }
+    
+
+    delete []arr;
+    return 0;
+}
