8d3b7a97b60efa7d634df093d8281680e3eecd20
ekondrashov <Egorkondr4@gmail.com>
2021-12-29 01:28:06 +0300

    strings in cpp

---
+++ strings/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main()
+{
+
+    char string[] = {'h', 'e', 'l', 'l', 'o'. '\0'};
+
+    char string2[] = "hello";
+
+    std::cout << strlen(string) << std::endl;
+    std::cout << string2 << std::endl;
+
+
+    return 0;
+}
